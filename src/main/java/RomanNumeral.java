05dc35c1df3e49364c40a32b4ebf311c5fbdbf65
maicon <gugamaicon@gmail.com>
2021-07-21 12:18:26 -0300

    Implements roman to arabic conversion to base symbols

--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -1,6 +1,26 @@
+import java.util.Map;
+
 public class RomanNumeral {
 
+    private static final Map<String, Integer> romanToArabicMap =
+            Map.of(
+                    "I", 1,
+                    "V", 5,
+                    "X", 10,
+                    "L", 50,
+                    "C", 100,
+                    "D", 500,
+                    "M", 1000
+            );
+
     public static String toRoman(int arabicNumber){
-        return null;
+        String result= null;
+
+        for (var romanToArabicEntry : romanToArabicMap.entrySet()) {
+            if(arabicNumber == romanToArabicEntry.getValue()){
+                result = romanToArabicEntry.getKey();
+            }
+        }
+        return result;
     }
 }
--- src/test/java/RomanNumeralTest.java
+++ src/test/java/RomanNumeralTest.java
@@ -1,13 +1,42 @@
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class RomanNumeralTest {
 
     @Test
     void shouldConvert1ToRoman(){
         assertThat(RomanNumeral.toRoman(1)).isEqualTo("I");
+    }
+
+    @Test
+    void shouldConvert5ToRoman(){
+        assertThat(RomanNumeral.toRoman(5)).isEqualTo("V");
+    }
+
+    @Test
+    void shouldConvert10ToRoman(){
+        assertThat(RomanNumeral.toRoman(10)).isEqualTo("X");
+    }
+
+    @Test
+    void shouldConvert50ToRoman(){
+        assertThat(RomanNumeral.toRoman(50)).isEqualTo("L");
+    }
+
+    @Test
+    void shouldConvert100ToRoman(){
+        assertThat(RomanNumeral.toRoman(100)).isEqualTo("C");
+    }
+
+    @Test
+    void shouldConvert500ToRoman(){
+        assertThat(RomanNumeral.toRoman(500)).isEqualTo("D");
+    }
+
+    @Test
+    void shouldConvert1000ToRoman(){
+        assertThat(RomanNumeral.toRoman(1000)).isEqualTo("M");
     }
 
 }
